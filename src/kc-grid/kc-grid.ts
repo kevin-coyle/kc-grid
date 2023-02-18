99923f26b0b43ca0f52b51448202c80fb03cdd07
Kevin Coyle <kevin.coyle@drutek.co.uk>
2023-02-18 12:17:14 +0000

    Housekeeping

--- src/kc-grid/kc-grid.ts
+++ src/kc-grid/kc-grid.ts
@@ -10,9 +10,28 @@ export class KcGrid extends LitElement {
   @property()
   variant: "1" | "1-3" | "1-4" | "1-2" = "1";
 
+  @property({ type: Number })
+  smallMobileBreakpoint = 320;
+
+  @property({ type: Number })
+  mobileBreakpoint = 480;
+
+  @property({ type: Number })
+  tabletBreakpoint = 768;
+
+  @property({ type: Number })
+  desktopBreakpoint = 1024;
+
+  @property({ type: Number })
+  largeDesktopBreakpoint = 1280;
+
+  @property({ type: Boolean })
+  hasLayoutContainer = false;
+
   private classes = {
     "kc-grid": true,
     [`kc-grid--${this.variant}`]: true,
+    "kc-grid--has-layout-container": this.hasLayoutContainer,
   };
 
   render() {
