c4dc2b8086aa839fc65bc3d57467e9b782e0df04
Kevin Coyle <kevin.coyle@drutek.co.uk>
2023-02-18 14:41:03 +0000

    Second test release

--- types/kc-grid.stories.d.ts
+++ types/kc-grid.stories.d.ts
@@ -3,6 +3,7 @@ import "./kc-grid";
 declare const _default: Meta<import("@storybook/web-components").Args>;
 export default _default;
 export declare const Default: StoryObj;
-export declare const ThreeCol: StoryObj;
 export declare const Two: StoryObj;
+export declare const ThreeCol: StoryObj;
+export declare const FourCol: StoryObj;
 export declare const CustomBreakpoints: StoryObj;
